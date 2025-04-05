9d33fbf8fb0ff6db50f209e9e7eefaf6335f53ac
Tarus_M <marktarus01@gmail.com>
2025-04-05 17:50:17 +0300

    modifies the bst and the test file

--- BST.py
+++ BST.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.right = None
     
     def __str__(self):
-        return str(self.info)
+        return str(self.value)
 
 class BinarySearchTree:
     def __init__(self):
@@ -15,32 +15,99 @@ class BinarySearchTree:
     
     def insert(self, value):
         """Insert a value into the BST."""
-        pass  # TODO: Implement this method
+        def insert_node(node, value):
+            if node is None:
+                return TreeNode(value)
+            if value < node.value:
+                node.left = insert_node(node.left, value)
+            else:
+                node.right = insert_node(node.right, value)
+            return node
+
+        self.root = insert_node(self.root, value)
     
     def search(self, value):
         """Search for a value in the BST. Return True if found, else False."""
-        pass  # TODO: Implement this method
+        current = self.root
+        while current:
+            if current.value == value:
+                return True
+            elif value < current.value:
+                current = current.left
+            else:
+                current = current.right
+        return False
     
     def inorder_traversal(self):
         """Return a list of values representing in-order traversal."""
-        pass  # TODO: Implement this method
+        result = []
+
+        def traverse(node):
+            if node is not None:
+                traverse(node.left)
+                result.append(node.value)
+                traverse(node.right)
+        
+        traverse(self.root)
+        return result
+    
     
     def preorder_traversal(self):
         """Return a list of values representing pre-order traversal."""
-        pass  # TODO: Implement this method
+        result = []
+
+        def traverse(node):
+            if node is not None:
+                result.append(node.value)
+                traverse(node.left)
+                traverse(node.right)
+        
+        traverse(self.root)
+        return result
     
     def postorder_traversal(self):
         """Return a list of values representing post-order traversal."""
-        pass  # TODO: Implement this method
+        result = []
+
+        def traverse(node):
+            if node is not None:
+                traverse(node.left)
+                traverse(node.right)
+                result.append(node.value)
+        
+        traverse(self.root)
+        return result
     
     def delete(self, value):
         """Delete a value from the BST."""
-        pass  # TODO: Implement this method
+        def find_min(node):
+            while node.left is not None:
+                node = node.left
+            return node
+
+        def delete_node(node, value):
+            if node is None:
+                return node
+            if value < node.value:
+                node.left = delete_node(node.left, value)
+            elif value > node.value:
+                node.right = delete_node(node.right, value)
+            else:
+                if node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+                temp = find_min(node.right)
+                node.value = temp.value
+                node.right = delete_node(node.right, temp.value)
+            return node
+
+        self.root = delete_node(self.root, value)
     
 # Example usage:
-# bst = BinarySearchTree()
-# bst.insert(10)
-# bst.insert(5)
-# bst.insert(15)
-# print(bst.search(10))  # Expected output: True
-# print(bst.inorder_traversal())  # Expected output: [5, 10, 15]
+bst = BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+print(bst.search(10))  # Expected output: True
+print(bst.inorder_traversal())  # Expected output: [5, 10, 15]
